4762d719f48849890d3def9c85d7ab44c4815607
Mariusz Kępa <TrinityScorpion>
2021-07-11 15:05:05 +0200

    Init Commit

---
+++ src/test/java/com/example/CodersLabLastProject/CodersLabLastProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodersLabLastProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodersLabLastProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
